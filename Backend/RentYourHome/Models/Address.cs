1030303ef7131b8729b0dd1558a398f7b7b77564
kC0d3 <kC0d3@github.com>
2024-01-30 13:27:48 +0100

    fix: Remove unnecesary properties.

--- Backend/RentYourHome/Models/Address.cs
+++ Backend/RentYourHome/Models/Address.cs
@@ -2,6 +2,7 @@ namespace RentYourHome.Models;
 
 public class Address
 {
+    public int Id { get; init; }
     public string ZipCode { get; init; }
     public string City { get; init; }
     public string Street { get; init; }
