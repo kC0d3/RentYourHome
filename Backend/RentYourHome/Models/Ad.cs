85debb357ba200352a04c71ed8ee22afa33975a0
kC0d3 <kC0d3@github.com>
2024-01-30 11:21:27 +0100

    fix: Rename UserAppliedAd collection.

--- Backend/RentYourHome/Models/Ad.cs
+++ Backend/RentYourHome/Models/Ad.cs
@@ -1,7 +1,5 @@
 namespace RentYourHome.Models;
 
-
-
 public class Ad
 {
     public int Id { get; init; }
@@ -11,9 +9,6 @@ public class Ad
     public int Price { get; init; }
     public string Description { get; init; }
     public ICollection<Image> Images { get; init; }
-    
-    public ICollection<UserAdApply> AppliedAds { get; init; }
 
-    public int UserId { get; init; }
-    public User User { get; init; }
+    public ICollection<UserAppliedAd> AppliedAds { get; init; }
 }
