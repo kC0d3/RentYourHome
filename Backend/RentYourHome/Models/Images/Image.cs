23eb2f656782c916c00705a60d64c2db86cee6dc
kC0d3 <kC0d3@github.com>
2024-01-31 15:27:10 +0100

    refactor: Refactor Images classes.

--- Backend/RentYourHome/Models/Images/Image.cs
+++ Backend/RentYourHome/Models/Images/Image.cs
@@ -3,6 +3,5 @@ namespace RentYourHome.Models.Images;
 public class Image
 {
     public int Id { get; set; }
-    public byte[] ImageData { get; set; }
     public string FileName { get; set; }
 }
--- Backend/RentYourHome/Models/Images/ImageDto.cs
+++ Backend/RentYourHome/Models/Images/ImageDto.cs
@@ -2,6 +2,5 @@ namespace RentYourHome.Models.Images;
 
 public class ImageDto
 {
-    public byte[] ImageData { get; set; }
     public string FileName { get; set; }
 }
