638d4ae8602b06cdfb5735d4484e5c4ed1f3a028
kC0d3 <kC0d3@github.com>
2024-01-31 11:24:13 +0100

    feat: Add AddressDto class.

---
+++ Backend/RentYourHome/Models/AddressDto.cs
@@ -0,0 +1,9 @@
+namespace RentYourHome.Models;
+
+public class AddressDto
+{
+    public string ZipCode { get; init; }
+    public string City { get; init; }
+    public string Street { get; init; }
+    public string HouseNumber { get; init; }
+}
