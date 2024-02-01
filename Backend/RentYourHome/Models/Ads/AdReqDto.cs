9cb02579d927ade176222442de0e2c543b52ea32
kC0d3 <kC0d3@github.com>
2024-02-01 10:35:36 +0100

    feat: Add AdReqDto class.

---
+++ Backend/RentYourHome/Models/Ads/AdReqDto.cs
@@ -0,0 +1,14 @@
+using RentYourHome.Models.Addresses;
+
+namespace RentYourHome.Models.Ads;
+
+public class AdReqDto
+{
+    public AddressDto Address { get; init; }
+    public int Rooms { get; init; }
+    public int Size { get; init; }
+    public int Price { get; init; }
+    public string Description { get; init; }
+    public ICollection<string> Images { get; init; }
+    public int UserId { get; init; }
+}
