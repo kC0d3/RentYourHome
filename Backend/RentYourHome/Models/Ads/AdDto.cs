91f50ee90bd6198258f9321d4fa4deffc6b017b9
kC0d3 <kC0d3@github.com>
2024-02-12 11:58:32 +0100

    refactor: Add Approved and Id property to AdDto class.

--- Backend/RentYourHome/Models/Ads/AdDto.cs
+++ Backend/RentYourHome/Models/Ads/AdDto.cs
@@ -5,10 +5,12 @@ namespace RentYourHome.Models.Ads;
 
 public class AdDto
 {
+    public int Id { get; init; }
     public AddressDto Address { get; init; }
     public int Rooms { get; init; }
     public int Size { get; init; }
     public int Price { get; init; }
+    public bool Approved { get; init; }
     public string Description { get; init; }
     public ICollection<string> Images { get; init; }
 }
