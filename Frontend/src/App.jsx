bf202b720592df232ea774c470cf0beb66701f5d
Daniel Kiss <danielkiss910@gmail.com>
2024-02-12 12:19:26 +0100

    feat: Add ad details page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from "./Components/HomePage.jsx";
 import LoginPage from './Components/LoginPage.jsx';
+import AdDetails from './Components/AdDetails.jsx';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/ads/:id" element={<AdDetails />} />
       </Routes>
     </Router>
   );
