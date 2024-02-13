067e3297d5fc717e562f13d7357aa59c8b5ecd85
Daniel Kiss <danielkiss910@gmail.com>
2024-02-13 15:06:31 +0100

    feat: Add new data to login page

--- Frontend/src/Components/LoginPage.jsx
+++ Frontend/src/Components/LoginPage.jsx
@@ -1,16 +1,48 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar.jsx";
 import RegistrationForm from "./RegistrationForm";
 
 function LoginPage() {
+    const navigate = useNavigate();
     const [showRegistration, setShowRegistration] = useState(false);
     const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [loginError, setLoginError] = useState(null);
 
-    const handleLoginSubmit = () => {
-        // TODO: Check credentials / Authorize
-        setIsLoggedIn(true)
+    const handleLoginSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                }),
+            });
+            console.log(username);
+            console.log(password);
+
+            if (response.ok) {
+                const data = await response.JSON();
+                setIsLoggedIn(true);
+                console.log('User successfully logged in.');
+                setTimeout(() => {
+                    navigate('/');
+                }, 3000);
+            } else {
+                setLoginError('Invalid username or password. Please try agagin.');
+                console.error('Invalid username or password during login.');
+            }
+        } catch (error) {
+            setLoginError('Invalid username or password. Please try agagin.');
+            console.error('Invalid username or password during login.');
+        }
     }
 
     const handleRegisterClick = () => {
@@ -24,8 +56,8 @@ function LoginPage() {
                 {isLoggedIn ? (<p>Welcome, {username}</p>) : (<p>Please log in to access the full website.</p>)}
                 <div className="login-form">
                     <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-                    <input type="password" placeholder="Password" />
-                    <button>Log in</button>
+                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <button onClick={handleLoginSubmit}>Log in</button>
                     <p>Not a member yet? <span onClick={handleRegisterClick} style={{ textDecoration: "underline", cursor: "pointer" }}>Click here to Register</span></p>
                 </div>
                 {showRegistration && <RegistrationForm setShowRegistration={setShowRegistration} />}
