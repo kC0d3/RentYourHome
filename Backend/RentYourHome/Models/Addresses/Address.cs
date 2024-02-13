ae04bfc30d30aef2f6e8ee2b8793a48ac0574e00
kC0d3 <kC0d3@github.com>
2024-02-13 11:29:16 +0100

    refactor: Refactor Address class.

--- Backend/RentYourHome/Models/Addresses/Address.cs
+++ Backend/RentYourHome/Models/Addresses/Address.cs
@@ -1,10 +1,15 @@
+using RentYourHome.Models.Ads;
+
 namespace RentYourHome.Models.Addresses;
 
 public class Address
 {
     public int Id { get; init; }
-    public string ZipCode { get; init; }
-    public string City { get; init; }
-    public string Street { get; init; }
-    public string HouseNumber { get; init; }
+    public string ZipCode { get; set; }
+    public string City { get; set; }
+    public string Street { get; set; }
+    public string HouseNumber { get; set; }
+
+    public Ad Ad { get; init; }
+    public int AdId { get; init; }
 }
