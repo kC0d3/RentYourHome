3a1e5e4dd44f5501819c9d7ec3fe3f6a43bb78fd
GaborToth22 <toccs22@gmail.com>
2024-02-14 18:21:09 +0100

    refactor: ProfilPage

--- Frontend/src/Components/ProfilePage.jsx
+++ Frontend/src/Components/ProfilePage.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import ProfileInfo from "./ProfileInfo";
+import ProfileAds from "./ProfilAds";
 
 function ProfilePage({loggedUser}) {
-console.log(loggedUser.firstName)
+console.log(loggedUser)
 
     return (
         <>
         <ProfileInfo loggedUser={loggedUser}/>
+        <div className="profil-page-container">
+            <div className="published-ads-container">
+                <h2 className="published-ads-title">Published Ads</h2>
+                    <ProfileAds className="published-ads" ads={loggedUser.publishedAds}/>
+            </div>
+            <div className="applied-ads-container">
+                <h2 className="applied-ads-title">Applied Ads</h2>
+                    <ProfileAds className="published-ads" ads={loggedUser.publishedAds}/>
+            </div>
+        </div>
         </>
     )
 }
