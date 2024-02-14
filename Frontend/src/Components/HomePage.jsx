ad9496b16e31ff70689b9d05bb2fea127a689b74
Daniel Kiss <danielkiss910@gmail.com>
2024-02-14 10:40:30 +0100

    feat: add filter reset to homepage component

--- Frontend/src/Components/HomePage.jsx
+++ Frontend/src/Components/HomePage.jsx
@@ -39,71 +39,86 @@ function HomePage() {
         let filteredAds = allAdsData;
 
         if (filters.location) {
-            filteredAds = filteredAds.filter(ad => 
+            filteredAds = filteredAds.filter(ad =>
                 ad.address.city.toLowerCase().includes(filters.location.toLowerCase()) ||
                 ad.address.street.toLowerCase().includes(filters.location.toLowerCase()) ||
                 ad.address.zipCode.toLowerCase().includes(filters.location));
         }
 
         if (filters.minPrice) {
-            filteredAds = filteredAds.filter(ad => 
+            filteredAds = filteredAds.filter(ad =>
                 ad.price >= filters.minPrice);
         }
 
         if (filters.maxPrice) {
-            filteredAds = filteredAds.filter(ad => 
+            filteredAds = filteredAds.filter(ad =>
                 ad.price <= filters.maxPrice);
         }
 
         if (filters.minSize) {
-            filteredAds = filteredAds.filter(ad => 
+            filteredAds = filteredAds.filter(ad =>
                 ad.size >= filters.minSize);
         }
 
         if (filters.maxSize) {
-            filteredAds = filteredAds.filter(ad => 
+            filteredAds = filteredAds.filter(ad =>
                 ad.size <= filters.maxSize);
         }
 
         if (filters.minRooms) {
-            filteredAds = filteredAds.filter(ad => 
+            filteredAds = filteredAds.filter(ad =>
                 ad.rooms >= filters.minRooms);
         }
 
         if (filters.maxRooms) {
-            filteredAds = filteredAds.filter(ad => 
+            filteredAds = filteredAds.filter(ad =>
                 ad.rooms <= filters.maxRooms);
         }
         setDisplayedAds(filteredAds);
     }
-    
+
+    const handleResetFilters = () => {
+        setFilters({
+            location: '',
+            minPrice: '',
+            maxPrice: '',
+            minSize: '',
+            maxSize: '',
+            minRooms: '',
+            maxRooms: '',
+        });
+    }
 
     return (
         <>
             <Navbar />
             <div className="homepage-container">
-                    <div className="filter-area">
-                        <FiltersBar onFilterChange={handleFilterChange} onSubmitFilters={handleApplyFilters} />
-                        <br></br>
-                            </div>
-                <div className="content-area">
-                            {displayedAds.map((ad, index) => (
-                                <div key={index} className="card" onClick={() => navigate(`/ads/${ad.id}`)}>
-                                    <div className="image-container">
-                                        {ad.images.map((imageName, index) => (
-                                            <img key={index} src={`/api/images/${imageName}`} alt="Ad" />
-                                        ))}
-                                    </div>
-                                    <div>Location: {`${ad.address.zipCode}, ${ad.address.city}, ${ad.address.street} ${ad.address.houseNumber}`}</div>
-                                    <div>Rooms: {ad.rooms}</div>
-                                    <div>Size: {ad.size} sqm</div>
-                                    <div>Price: {ad.price} HUF</div>
-                                    <div>Description: {ad.description}</div>
-                                    <br></br>
-                                </div>
-                            ))}
-                    </div>
+                <div className="filter-area">
+                    <FiltersBar 
+                        onFilterChange={handleFilterChange} 
+                        onSubmitFilters={handleApplyFilters} 
+                        onResetFilters={handleResetFilters}
+                    />
+                    <br></br>
                 </div>
+                <div className="content-area">
+                    {displayedAds.map((ad, index) => (
+                        <div key={index} className="card" onClick={() => navigate(`/ads/${ad.id}`)}>
+                            <div className="image-container">
+                                {ad.images.map((imageName, index) => (
+                                    <img key={index} src={`/api/images/${imageName}`} alt="Ad" />
+                                ))}
+                            </div>
+                            <div>Location: {`${ad.address.zipCode}, ${ad.address.city}, ${ad.address.street} ${ad.address.houseNumber}`}</div>
+                            <div>Rooms: {ad.rooms}</div>
+                            <div>Size: {ad.size} sqm</div>
+                            <div>Price: {ad.price} HUF</div>
+                            <div>Description: {ad.description}</div>
+                            <br></br>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </>
     );
 }
