2e43932eda61c9828ed8ddb56215a773ffecf14a
GaborToth22 <toccs22@gmail.com>
2024-02-14 16:59:05 +0100

    fix: ProfileBar + css

--- Frontend/src/Components/ProfileInfo.jsx
+++ Frontend/src/Components/ProfileInfo.jsx
@@ -3,8 +3,7 @@ function ProfileInfo({ loggedUser }) {
     
     return (
         <div className="profilebar">
-            <p><span>FirstName:</span> {loggedUser.firstName}</p>
-            <p><span>LastName:</span> {loggedUser.lastName}</p>
+            <p><span>Name:</span> {loggedUser.firstName} {loggedUser.lastName}</p>
             <p><span>Username:</span> {loggedUser.username}</p>
             <p><span>Email:</span> {loggedUser.email}</p>
         </div>
