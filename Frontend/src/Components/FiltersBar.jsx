a305e97ada5014c03bfa284db80343e6f38e0f1c
Daniel Kiss <danielkiss910@gmail.com>
2024-02-15 10:04:32 +0100

    feat: Reset filter input values as well when resetting filters

--- Frontend/src/Components/FiltersBar.jsx
+++ Frontend/src/Components/FiltersBar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-const FiltersBar = ({ onFilterChange, onSubmitFilters, onResetFilters }) => {
+const FiltersBar = ({ onFilterChange, onSubmitFilters, onResetFilters, filters }) => {
     return (
         <div className="filters">
             <div className="filter-input-group">
                 <input
                     type="text"
                     placeholder="Location"
+                    value={filters.location}
                     onChange={(e) => onFilterChange('location', e.target.value)}
                     className="filter-input location-input"
                 />
@@ -15,12 +16,14 @@ const FiltersBar = ({ onFilterChange, onSubmitFilters, onResetFilters }) => {
                 <input
                     type="number"
                     placeholder="Min Price"
+                    value={filters.minPrice}
                     onChange={(e) => onFilterChange('minPrice', e.target.value)}
                     className="filter-input"
                 />
                 <input
                     type="number"
                     placeholder="Max Price"
+                    value={filters.maxPrice}
                     onChange={(e) => onFilterChange('maxPrice', e.target.value)}
                     className="filter-input"
                 />
@@ -29,12 +32,14 @@ const FiltersBar = ({ onFilterChange, onSubmitFilters, onResetFilters }) => {
                 <input
                     type="number"
                     placeholder="Min Size"
+                    value={filters.minSize}
                     onChange={(e) => onFilterChange('minSize', e.target.value)}
                     className="filter-input"
                 />
                 <input
                     type="number"
                     placeholder="Max Size"
+                    value={filters.maxSize}
                     onChange={(e) => onFilterChange('maxSize', e.target.value)}
                     className="filter-input"
                 />
@@ -43,12 +48,14 @@ const FiltersBar = ({ onFilterChange, onSubmitFilters, onResetFilters }) => {
                 <input
                     type="number"
                     placeholder="Min Rooms"
+                    value={filters.minRooms}
                     onChange={(e) => onFilterChange('minRooms', e.target.value)}
                     className="filter-input"
                 />
                 <input
                     type="number"
                     placeholder="Max Rooms"
+                    value={filters.maxRooms}
                     onChange={(e) => onFilterChange('maxRooms', e.target.value)}
                     className="filter-input"
                 />
