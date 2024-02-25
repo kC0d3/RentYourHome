f50d9f2fa2d071b670eb3a16b21199d79319386e
GaborToth22 <toccs22@gmail.com>
2024-02-25 11:09:58 +0100

    refactor: put IssuerSigningKey to .env  and .env.example

--- Backend/RentYourHome/Program.cs
+++ Backend/RentYourHome/Program.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using DotNetEnv;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
@@ -16,6 +17,9 @@ ConfigureSwagger();
 AddDbContext();
 AddAuthentication();
 AddIdentity();
+
+var dotenv = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", ".env");
+Env.Load(dotenv);
 
 var app = builder.Build();
 
@@ -63,7 +67,7 @@ void AddAuthentication()
                 ValidAudience = builder.Configuration["JwtSettings:ValidAudience"],
                 IssuerSigningKey = new SymmetricSecurityKey(
                     Encoding.UTF8.GetBytes(
-                        builder.Configuration["JwtSettings:IssuerSigningKey"]) //<- ezt hol kéne pontosan tárolni?
+                        Environment.GetEnvironmentVariable("ISSUERSIGNINGKEY"))
                 ),
             };
 
