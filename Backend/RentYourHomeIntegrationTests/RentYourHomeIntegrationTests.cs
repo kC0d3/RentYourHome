41d86fea6bbcc351bba01f3cbfb3eacddfb5e8b8
kC0d3 <kC0d3@github.com>
2024-02-27 15:00:48 +0100

    feat: Add CreateAd integration test.

--- Backend/RentYourHomeIntegrationTests/RentYourHomeIntegrationTests.cs
+++ Backend/RentYourHomeIntegrationTests/RentYourHomeIntegrationTests.cs
@@ -1,7 +1,11 @@
 using System.Diagnostics;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using FluentAssertions;
+using Microsoft.AspNetCore.Http.HttpResults;
 using RentYourHome.Contracts;
+using RentYourHome.Models.Addresses;
+using RentYourHome.Models.Ads;
 using RentYourHome.Services.Authentication;
 
 namespace RentYourHomeIntegrationTests;
@@ -56,5 +60,49 @@ public class RentYourHomeIntegrationTests
 
         userResponse?.Should().Be("User");
         token.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task CreateAd()
+    {
+        //Arrange
+        var application = new RentYourHomeWebApplicationFactory();
+        var userReg = new RegistrationRequest("user1@user1.com", "user1", "firstname", "lastname", "123456");
+        var user = new AuthRequest("user1", "123456");
+        var ad = new AdReqDto
+        {
+            Address = new AddressDto
+            {
+                City = "City",
+                HouseNumber = "HouseNumber",
+                Street = "Street",
+                ZipCode = "ZipCode"
+            },
+            Description = "Description",
+            Images = new[] { "1.jpg" },
+            Price = 1,
+            Rooms = 1,
+            Size = 1,
+            UserId = 1
+        };
+
+        var client = application.CreateClient();
+        await client.PostAsJsonAsync("/api/auth/register", userReg);
+        var logResp = await client.PostAsJsonAsync("/api/auth/login", user);
+        var cookies = logResp.Headers.GetValues("Set-Cookie");
+        var token = cookies
+            .SelectMany(cookie => cookie.Split(';'))
+            .FirstOrDefault(c => c.Contains("token="))?
+            .Split('=')[1];
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+
+        //Act
+        var response = await client.PostAsJsonAsync("/api/ads", ad);
+
+        //Asserrt
+        response.EnsureSuccessStatusCode();
+        var adResponse = await response.Content.ReadFromJsonAsync<AdReqDto>();
+        adResponse?.UserId.Should().Be(1);
     }
 }
