fa5cf32722b90c21cdf7dc64deaa8f2656c81587
kC0d3 <kC0d3@github.com>
2024-02-27 13:29:39 +0100

    refactor: Refactor AdRepository.

--- Backend/RentYourHome/Repositories/AdRepository/AdRepository.cs
+++ Backend/RentYourHome/Repositories/AdRepository/AdRepository.cs
@@ -8,46 +8,43 @@ namespace RentYourHome.Repositories.AdRepository;
 public class AdRepository : IAdRepository
 {
     private readonly IClassConverterService _classConverterService;
+    private readonly DatabaseContext _dbContext;
 
-    public AdRepository(IClassConverterService classConverterService)
+    public AdRepository(DatabaseContext dbContext, IClassConverterService classConverterService)
     {
         _classConverterService = classConverterService;
+        _dbContext = dbContext;
     }
 
     public void AddAdToDb(AdReqDto ad)
     {
-        using var dbContext = new DatabaseContext();
-        dbContext.Add(_classConverterService.AdReqDtoToAd(ad));
-        dbContext.SaveChanges();
+        _dbContext.Add(_classConverterService.AdReqDtoToAd(ad));
+        _dbContext.SaveChanges();
     }
 
     public async Task<IEnumerable<AdDto>> GetAllAds()
     {
-        await using var dbContext = new DatabaseContext();
-        return _classConverterService.AdsToAdDtos(dbContext.Ads
+        return _classConverterService.AdsToAdDtos(_dbContext.Ads
             .Include(a => a.Images)
             .Include(a => a.Address).ToList());
     }
 
     public async Task<Ad?> GetAdById(int id)
     {
-        await using var dbContext = new DatabaseContext();
-        return dbContext.Ads.Include(a => a.Address)
+        return _dbContext.Ads.Include(a => a.Address)
             .Include(a => a.Images)
             .FirstOrDefault(a => a.Id == id);
     }
 
     public void UpdateAd(Ad ad)
     {
-        using var dbContext = new DatabaseContext();
-        dbContext.Update(ad);
-        dbContext.SaveChanges();
+        _dbContext.Update(ad);
+        _dbContext.SaveChanges();
     }
 
     public void DeleteAd(Ad ad)
     {
-        using var dbContext = new DatabaseContext();
-        dbContext.Remove(ad);
-        dbContext.SaveChanges();
+        _dbContext.Remove(ad);
+        _dbContext.SaveChanges();
     }
 }
