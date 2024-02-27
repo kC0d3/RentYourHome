41f0ad97d33a831563e6da7eb3d3b9261a2b1b7a
kC0d3 <kC0d3@github.com>
2024-02-27 13:28:50 +0100

    refactor: Refactor UsersContext.

--- Backend/RentYourHome/Data/UsersContext.cs
+++ Backend/RentYourHome/Data/UsersContext.cs
@@ -7,17 +7,11 @@ namespace RentYourHome.Data;
 
 public class UsersContext : IdentityDbContext<IdentityUser, IdentityRole, string>
 {
-    public UsersContext (DbContextOptions<UsersContext> options)
+    public UsersContext(DbContextOptions<UsersContext> options)
         : base(options)
     {
     }
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    {
-        var dotenv = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", ".env");
-        Env.Load(dotenv);
-        options.UseSqlServer($"Server={Environment.GetEnvironmentVariable("DBHOST")},{Environment.GetEnvironmentVariable("DBPORT")};Database={Environment.GetEnvironmentVariable("DBNAME")};User Id={Environment.GetEnvironmentVariable("DBUSER")};Password={Environment.GetEnvironmentVariable("DBPASSWORD")};Encrypt=false;TrustServerCertificate=true;");
 
-    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
