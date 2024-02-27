2622f6540f2fae4255fb656d2fcca1dd873e9040
kC0d3 <kC0d3@github.com>
2024-02-27 15:29:27 +0100

    feat: Add ApproveAd integration test.

--- Backend/RentYourHomeIntegrationTests/RentYourHomeIntegrationTests.cs
+++ Backend/RentYourHomeIntegrationTests/RentYourHomeIntegrationTests.cs
@@ -105,4 +105,62 @@ public class RentYourHomeIntegrationTests
         var adResponse = await response.Content.ReadFromJsonAsync<AdReqDto>();
         adResponse?.UserId.Should().Be(1);
     }
+
+    [Fact]
+    public async Task ApproveAd()
+    {
+        //Arrange
+        var application = new RentYourHomeWebApplicationFactory();
+        var userReg = new RegistrationRequest("user1@user1.com", "user1", "firstname", "lastname", "123456");
+        var admin = new AuthRequest("admin", "admin123");
+        var user = new AuthRequest("user1", "123456");
+        var ad = new AdReqDto
+        {
+            Address = new AddressDto
+            {
+                City = "City",
+                HouseNumber = "HouseNumber",
+                Street = "Street",
+                ZipCode = "ZipCode"
+            },
+            Description = "Description",
+            Images = new[] { "1.jpg" },
+            Price = 1,
+            Rooms = 1,
+            Size = 1,
+            UserId = 1
+        };
+
+        var client = application.CreateClient();
+        //User reg, login & logout
+        await client.PostAsJsonAsync("/api/auth/register", userReg);
+        var userLogResp = await client.PostAsJsonAsync("/api/auth/login", user);
+        var userCookies = userLogResp.Headers.GetValues("Set-Cookie");
+        var userToken = userCookies
+            .SelectMany(cookie => cookie.Split(';'))
+            .FirstOrDefault(c => c.Contains("token="))?
+            .Split('=')[1];
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", userToken);
+        await client.PostAsJsonAsync("/api/ads", ad);
+        await client.PostAsync("/api/auth/logout", null);
+
+        //Admin login
+        var adminLogResp = await client.PostAsJsonAsync("/api/auth/login", admin);
+        var adminCookies = adminLogResp.Headers.GetValues("Set-Cookie");
+        var adminToken = adminCookies
+            .SelectMany(cookie => cookie.Split(';'))
+            .FirstOrDefault(c => c.Contains("token="))?
+            .Split('=')[1];
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", adminToken);
+
+        //Act
+        await client.PutAsync("/api/ads/approve/1", null);
+        var response = await client.GetAsync("/api/ads/1");
+
+        //Asserrt
+        response.EnsureSuccessStatusCode();
+        var adResponse = await response.Content.ReadFromJsonAsync<AdDto>();
+        adResponse?.Id.Should().Be(1);
+        adResponse?.Approved.Should().Be(true);
+    }
 }
