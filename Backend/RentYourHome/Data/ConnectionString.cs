151144299715f64373806915ff39e1f7d7b3767b
kC0d3 <kC0d3@github.com>
2024-02-29 10:23:19 +0100

    feat: Add GetLiveConnectionString method.

--- Backend/RentYourHome/Data/ConnectionString.cs
+++ Backend/RentYourHome/Data/ConnectionString.cs
@@ -13,6 +13,15 @@ public static class ConnectionString
             $"Server={Environment.GetEnvironmentVariable("DBHOST")},{Environment.GetEnvironmentVariable("DBPORT")};Database={Environment.GetEnvironmentVariable("DBNAME")};User Id={Environment.GetEnvironmentVariable("DBUSER")};Password={Environment.GetEnvironmentVariable("DBPASSWORD")};Encrypt=false;";
     }
 
+    public static string GetLiveConnectionString()
+    {
+        var root = Directory.GetCurrentDirectory();
+        var dotenv = Path.Combine(root, "..", "..", ".env");
+        Env.Load(dotenv);
+        return
+            $"Server={Environment.GetEnvironmentVariable("LIVESERVERHOST")},{Environment.GetEnvironmentVariable("DBPORT")};Initial Catalog={Environment.GetEnvironmentVariable("INITIALCATALOG")};User ID={Environment.GetEnvironmentVariable("LIVEDBUSER")};Password={Environment.GetEnvironmentVariable("DBPASSWORD")};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+    }
+
     public static string GetTestConnectionStringForFactory()
     {
         var root = Directory.GetCurrentDirectory();
