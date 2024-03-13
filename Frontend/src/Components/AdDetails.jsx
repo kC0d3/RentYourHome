e71b681d9eb1bfe3818706148fe7669eaa00f966
kC0d3 <kC0d3@github.com>
2024-03-13 13:44:44 +0100

    feat: Add Apply or Cancel button.

--- Frontend/src/Components/AdDetails.jsx
+++ Frontend/src/Components/AdDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 
-function AdDetails() {
+function AdDetails({ loggedUser, setLoggedUser, role }) {
     const { id } = useParams(); // Get ID from URL parameter
     const [adDetails, setAdDetails] = useState(null);
 
@@ -16,14 +16,54 @@ function AdDetails() {
             });
     }, [id]); // Run effect when ID changes
 
+    const fetchData = async () => {
+        try {
+            const res = await fetch(`/api/users/${loggedUser.username}`);
+            const data = await res.json();
+            setLoggedUser(data);
+        } catch (error) {
+            console.log("Error fetching data:", error);
+        }
+    };
+
+    const handleApplyAd = adId => {
+        fetch(`/api/useradapplication/apply/${adId}&${loggedUser.id}`, {
+            method: 'POST'
+        })
+            .then(res => {
+                if (res.ok) {
+                    fetchData();
+                }
+                else {
+                    console.error(`Error apply ad.`);
+                }
+            })
+            .catch(error => console.log("Error apply ad:", error));
+    }
+
+    const handleCancelApply = adId => {
+        fetch(`/api/useradapplication/cancel/${adId}&${loggedUser.id}`, {
+            method: 'DELETE'
+        })
+            .then(res => {
+                if (res.ok) {
+                    fetchData();
+                }
+                else {
+                    console.error(`Error cancel apply.`);
+                }
+            })
+            .catch(error => console.log("Error cancel apply:", error));
+    }
+
     if (!adDetails) {
         return <div><h2>Loading...</h2></div>;
     }
 
     const imageGalleryClass = `image-gallery ${adDetails.images.length === 1 ? "one-image" :
-            adDetails.images.length === 2 ? "two-images" :
-                adDetails.images.length === 3 ? "three-images" :
-                    "multi-image"
+        adDetails.images.length === 2 ? "two-images" :
+            adDetails.images.length === 3 ? "three-images" :
+                "multi-image"
         }`;
 
     return (
@@ -40,6 +80,7 @@ function AdDetails() {
                 <div>Size: {adDetails.size} sqm</div>
                 <div>Price: {adDetails.price} HUF</div>
                 <div>Description: {adDetails.description}</div>
+                {role !== "Admin" && loggedUser && !loggedUser.publishedAds.some(publishedAd => publishedAd.id === adDetails.id) ? !loggedUser.userAdApplications.some(applicaton => applicaton.adId === adDetails.id) ? <button onClick={() => handleApplyAd(adDetails.id)}>Apply</button> : <button onClick={() => handleCancelApply(adDetails.id)}>Cancel</button> : null}
             </div>
             <div className="cancel-button">
                 <Link to="/">
